4be27292a815245805b553c045242d9217654503
Dmitriy S <sutyginds@gmail.com>
2020-02-23 19:54:11 +0400

    init: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == null||array.length == 0) {
+        return 0;
+    }
+  array.sort(compareNumeric);
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == null||array.length == 0) {
+        return 0;
+    }
+    array.sort(compareNumeric);
+  return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == null||array.length == 0) {
+        return 0;
+    }
+    res = 0;
+    for (i=0; i<array.length; i++) {
+        res = res + array[i];
+    }
+  return res/array.length;
 }
+
+function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+  }
+  
